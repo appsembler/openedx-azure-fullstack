06e96efb816b2bb4ae70729c13be5938a286cd51
TJ Keemon <keems21@gmail.com>
2015-09-30 09:47:11 -0400

    first attempt at arm templates for vagrant-fullstack

---
+++ configure-openedx.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+# print commands and arguments as they are executed
+set -x
+
+echo "Starting Open edX fullstack install on pid $$"
+date
+ps axjf
+
+#############
+# Parameters
+#############
+
+AZUREUSER=$1
+HOMEDIR="/home/$AZUREUSER"
+VMNAME=`hostname`
+echo "User: $AZUREUSER"
+echo "User home dir: $HOMEDIR"
+echo "vmname: $VMNAME"
+
+###################
+# Common Functions
+###################
+
+ensureAzureNetwork()
+{
+  # ensure the host name is resolvable
+  hostResolveHealthy=1
+  for i in {1..120}; do
+    host $VMNAME
+    if [ $? -eq 0 ]
+    then
+      # hostname has been found continue
+      hostResolveHealthy=0
+      echo "the host name resolves"
+      break
+    fi
+    sleep 1
+  done
+  if [ $hostResolveHealthy -ne 0 ]
+  then
+    echo "host name does not resolve, aborting install"
+    exit 1
+  fi
+
+  # ensure the network works
+  networkHealthy=1
+  for i in {1..12}; do
+    wget -O/dev/null http://bing.com
+    if [ $? -eq 0 ]
+    then
+      # hostname has been found continue
+      networkHealthy=0
+      echo "the network is healthy"
+      break
+    fi
+    sleep 10
+  done
+  if [ $networkHealthy -ne 0 ]
+  then
+    echo "the network is not healthy, aborting install"
+    ifconfig
+    ip a
+    exit 2
+  fi
+}
+ensureAzureNetwork
+
+###################################################
+# Update Ubuntu and install prereqs
+###################################################
+
+time sudo apt-get -y update && sudo apt-get -y upgrade
+time sudo apt-get install -y build-essential software-properties-common python-software-properties curl git-core libxml2-dev libxslt1-dev libfreetype6-dev python-pip python-apt python-dev libxmlsec1-dev swig
+time sudo pip install --upgrade pip
+time sudo pip install --upgrade virtualenv
+
+
+###################################################
+# Download configuration repo and start ansible
+###################################################
+
+cd /tmp
+time git clone https://github.com/appsembler/configuration.git
+cd configuration
+time git checkout appsembler/azureDeploy
+time sudo pip install -r requirements.txt
+cd playbooks
+
+curl https://raw.githubusercontent.com/tkeemon/openedx-azure-fullstack/master/server-vars.yml > /tmp/server-vars.yml
+
+sudo ansible-playbook -i localhost, -c local vagrant-fullstack.yml -e@/tmp/server-vars.yml
+
+date
+echo "Completed Open edX fullstack provision on pid $$"
